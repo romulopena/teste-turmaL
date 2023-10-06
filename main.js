e4ccf70e2dd2c16e792da4b8fb6ff6789ad65e73
PROF ROMULO <rjlpena@gmail.com>
2023-10-05 21:07:37 -0300

    grilo

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function tocaSomRisada(){
     document.querySelector("#som_tecla_risada").play();
 }
 document.querySelector(".tecla_risada").onclick = tocaSomRisada;
+
+function tocaSomGrilo(){
+    document.querySelector("#som_tecla_grilo").play();
+}
+document.querySelector(".tecla_grilo").onclick = tocaSomGrilo;
