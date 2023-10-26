cd34a1469ed217d22b8bfdaa3ffa35129646943d
PROF ROMULO <rjlpena@gmail.com>
2023-10-26 20:40:59 -0300

    idElementoAudio

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 //criado a função para tocar o som e feito a busca da id do som
-function tocaSomRisada(){
-    document.querySelector("#som_tecla_risada").play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
-//chamada da função criada para tocar ao clique do mouse
-document.querySelector(".tecla_risada").onclick = tocaSomRisada;
-
 //Constante das teclas com busca de todos os botões da classe
 const listaDeTeclas = document.querySelectorAll('.tecla');
 listaDeTeclas[2].onclick = tocaSomRisada;
 
 let contador = 0;
-while(contador < 9){
+while(contador < listaDeTeclas.length){
     listaDeTeclas[contador].onclick = tocaSomRisada;
     contador = contador + 1;
     console.log(contador);
