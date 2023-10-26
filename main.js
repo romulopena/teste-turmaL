6ca404f9fddf3665f04f16c51368a59e02ebb2d1
PROF ROMULO <rjlpena@gmail.com>
2023-10-26 20:41:37 -0300

    tocaSom

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ listaDeTeclas[2].onclick = tocaSomRisada;
 
 let contador = 0;
 while(contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSomRisada;
+    listaDeTeclas[contador].onclick = tocaSom;
     contador = contador + 1;
     console.log(contador);
 }
