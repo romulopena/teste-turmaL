2d84035d69516d364d7ed14557ea6a5f0b19045d
PROF ROMULO <rjlpena@gmail.com>
2023-10-26 20:32:06 -0300

    contador

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+//criado a função para tocar o som e feito a busca da id do som
 function tocaSomRisada(){
     document.querySelector("#som_tecla_risada").play();
 }
+//chamada da função criada para tocar ao clique do mouse
 document.querySelector(".tecla_risada").onclick = tocaSomRisada;
 
+//Constante das teclas com busca de todos os botões da classe
 const listaDeTeclas = document.querySelectorAll('.tecla');
 listaDeTeclas[2].onclick = tocaSomRisada;
+
+let contador = 0;
+while(contador < 9){
+    listaDeTeclas[contador].onclick = tocaSomRisada;
+    contador = contador + 1;
+    console.log(contador);
+}
