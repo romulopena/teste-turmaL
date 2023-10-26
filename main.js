5dc2a9728c45950f9904dade264c31aa56007914
PROF ROMULO <rjlpena@gmail.com>
2023-10-26 20:54:53 -0300

    idAudio

--- main.js
+++ main.js
@@ -8,7 +8,11 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 while(contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSom;
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_" + efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom(idAudio)
+    }
     contador = contador + 1;
     console.log(contador);
 }
